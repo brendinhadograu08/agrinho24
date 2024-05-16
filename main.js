6add86ffc41eb9e5581879ae4e391cd1c549f287
brendinhadograu08 <129185529+brendinhadograu08@users.noreply.github.com>
2024-05-16 09:11:41 -0300

    IY

---
+++ main.js
@@ -0,0 +1,9 @@
+// Add your JavaScript code here
+
+// Example JavaScript function
+function greet(name) {
+    console.log(`Hello, ${name}!`);
+  }
+  
+  // Call the example function
+  greet('World');
